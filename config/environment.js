ec1301a9c051322661e8e2a2da04e30d5084ccc3
mtahafarooq <mtahafrq@gmail.com>
2021-09-04 23:00:51 +0500

    integarte mapbox static image api  and develop map component

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,8 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN =
+    'pk.eyJ1IjoidGFoYWZhcm9vcSIsImEiOiJja2p3bWx0Z2owa2RmMnBzZHNyMnhlMGtpIn0.6Lpa-Hs0TBXTVsOjCFNMkg';
+
   return ENV;
 };
